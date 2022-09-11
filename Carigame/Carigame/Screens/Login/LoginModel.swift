dae7b283c1783d075d63a16a3568c6ee107c8648
Hario Budiharjo <hario.budiharjo@amarbank.co.id>
2022-09-11 11:16:00 +0700

    feat(core): Add pod depedency and structure project

---
+++ Carigame/Carigame/Screens/Login/LoginModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginModel.swift
+//  Carigame
+//
+//  Created by Hario Budiharjo on 11/09/22.
+//
+
+import Foundation
+
+struct LoginModel {
+    let email: String
+    let password: String
+}
---
+++ Carigame/Pods/Target Support Files/Pods-Carigame-CarigameUITests/Pods-Carigame-CarigameUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Carigame_CarigameUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Carigame_CarigameUITests
+@end
